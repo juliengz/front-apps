e2ba93787b32b2b0e19dcf5651272800b15bb8a6
Julien Gonzalez <julien.gonzalez@thetribe.io>
2021-04-09 15:15:53 +0200

    WIP

--- clean.sh
+++
@@ -1,7 +0,0 @@
-rm -rf node_modules
-rm package-lock.json
-rm -rf packages/core/node_modules
-rm -rf packages/core/package-lock.json
-rm -rf packages/module-builder/node_modules
-rm -rf packages/module-builder/package-lock.json
-npm install
