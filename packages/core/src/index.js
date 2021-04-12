347284128a1135bc0711a81051fafeb79cbc1019
Julien Gonzalez <julien.gonzalez@thetribe.io>
2021-04-12 11:23:26 +0200

    Clean Core component

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -4,9 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 export default function Core({children}) {
     return (
         <BrowserRouter>
-            <quote>START CORE (I take care of the routing, and i can do much more !)</quote>
             {children}
-            <quote>END CORE</quote>
         </BrowserRouter>
     )
 }
