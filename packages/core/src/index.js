7a344ff844571c9f28c967f27422f44d3eee41af
Julien Gonzalez <julien.gonzalez@thetribe.io>
2021-04-12 11:24:24 +0200

    Clean Core component code

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 export default function Core({children}) {
     return (
